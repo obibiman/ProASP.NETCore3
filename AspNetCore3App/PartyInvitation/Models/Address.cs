714d45318c3c3dd2c8dd7c81eb40b65df1a79d7a
Maurice Muoneke <obibibam@yahoo.com>
2020-06-28 13:40:19 -0500

    Addding address file

---
+++ AspNetCore3App/PartyInvitation/Models/Address.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyInvitation.Models
+{
+    /// <summary>
+    /// </summary>
+    public class Address
+    {
+        /// <summary>
+        /// </summary>
+        public int AddressId { get; set; }
+
+        /// <summary>
+        /// </summary>
+        public string AddressLine1 { get; set; }
+
+        /// <summary>
+        /// </summary>
+        public string AddressLine2 { get; set; }
+
+        /// <summary>
+        /// </summary>
+        public string CityName { get; set; }
+
+        /// <summary>
+        /// </summary>
+        public string Country { get; set; }
+
+        /// <summary>
+        /// </summary>
+        public string StateCode { get; set; }
+
+        /// <summary>
+        /// </summary>
+        public virtual Student Student { get; set; }
+
+        /// <summary>
+        /// </summary>
+        public string ZipCode { get; set; }
+    }
+}
