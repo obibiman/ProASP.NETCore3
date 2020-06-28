871c9f19f862aa20119c44825cbf8e168d7423d2
Maurice Muoneke <obibibam@yahoo.com>
2020-06-28 11:29:29 -0500

    More validation

--- AspNetCore3App/PartyInvitation/Controllers/HomeController.cs
+++ AspNetCore3App/PartyInvitation/Controllers/HomeController.cs
@@ -28,8 +28,15 @@ namespace PartyInvitation.Controllers
         [HttpPost]
         public ViewResult RsvpForm(GuestResponse guestResponse)
         {
-            Repository.AddResponse(guestResponse);
-            return View("Thanks", guestResponse);
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
         }
 
         public ViewResult ListResponses()
